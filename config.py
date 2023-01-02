e6fc606fe997dc72b0f3313a08f0dbd841152bb5
ayeshanadira <109727112+ayeshanadira@users.noreply.github.com>
2023-01-02 16:30:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "database-1.cvy7wbz5p7ok.us-east-1.rds.amazonaws.com"
+customhost = "employee.cil4uwyoii6v.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "teechizhe-bucket"
+custombucket = "ayeshanadirabintiirwan-bucket"
 customregion = "us-east-1"
